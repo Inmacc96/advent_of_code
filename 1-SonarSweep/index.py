39c70852e4548edd8784a01404c4518decc2f13d
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-16 17:18:17 +0100

    Solve the problem of day 1.

---
+++ 1-SonarSweep/index.py
@@ -0,0 +1,44 @@
+def readInput(filetxt):
+    f = open(filetxt,'r')
+    data = f.read().splitlines()
+    data = [int(x) for x in data]
+    f.close()
+    return data
+
+
+measurements = readInput('input.txt')
+
+def n_increases(measurements):
+    cont = 0
+    for (i,x) in enumerate(measurements):
+        if i!= len(measurements)-1 and measurements[i] < measurements[i+1]:
+            cont += 1
+
+    return cont
+
+print('Solution 1:' + n_increases(measurements))
+
+def n_sum_incresases(measurements):
+    cont=0
+    for (i,x) in enumerate(measurements):
+        if sum(measurements[i+1:i+4]) > sum(measurements[i:i+3]):
+            cont+=1
+    return cont
+
+#Otra forma
+def n_sum_incresases2(measurements):
+    cont=0
+    suma = sum(measurements[0:3])
+    for (i,x) in enumerate(measurements):
+        if sum(measurements[i:i+3]) > suma:
+            cont+=1
+        suma = sum(measurements[i:i+3])
+    return cont
+
+print('Solution 2:' + n_sum_incresases(measurements))
+print('Solution 2:' + n_sum_incresases2(measurements))
+
+
+
+
+ 
