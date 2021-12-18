ae51d2f09250c33c9396a145dbd67a1eae332919
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-18 12:29:54 +0100

    Solve part 2 of the problem of day 4.

--- 4-GiantSquid/index.py
+++ 4-GiantSquid/index.py
@@ -24,3 +24,28 @@ def winning_score(filetxt):
 print('Solution 1: {}'.format(winning_score('input.txt')))
 
 
+def last_winning_board_score(filetxt):
+    (list_num,list_boards) = readInput(filetxt)
+    len_total_boards=len(list_boards)
+    winning_list_boards = []
+    winning_nums = []
+    for num in list_num:
+        if len(winning_list_boards)!= len_total_boards:
+            for board in list_boards:
+                if board not in winning_list_boards:
+                    for row in board:
+                        if num in row:
+                            index = row.index(num)
+                            row[index]='-'
+                            if row == ['-']*5 or [num_rows[index] for num_rows in board] == ['-']*5:
+                                winning_nums.append(num)
+                                winning_list_boards.append(board)
+    sum_unmarked_number=sum([x for sub_list in winning_list_boards[-1] for x in sub_list if x!='-'])
+    return winning_nums[-1]*sum_unmarked_number
+
+print('Solution 2: {}'.format(last_winning_board_score('input.txt')))
+
+
+
+
+
